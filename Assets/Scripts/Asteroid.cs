5a648f2cee82d31e8d14074d89738eab3ecb3ac8
mamanelad <elad.maman@mail.huji.ac.il>
2022-05-17 19:47:19 +0300

    asteroid explosion

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using Object = System.Object;
 using Random = UnityEngine.Random;
 
 public class Asteroid : MonoBehaviour
 {
     private float tumble;
     [SerializeField] private float maxTumble = 20;
+    [SerializeField] private float timeToDestroy = 0.5f;
+    [SerializeField] private GameObject explosionEffect;
+    private bool hadExplode;
+    private GameObject currExplosion;
+
 
     void Start()
     {
         SetTumble();
         GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+    private void Update()
+    {
+        if (hadExplode)
+        {
+            timeToDestroy -= Time.deltaTime;
+            if (timeToDestroy <= 0)
+            {
+                DestroyAsteroid();
+            }
+        }
     }
 
     private void SetTumble()
@@ -26,16 +44,25 @@ public class Asteroid : MonoBehaviour
             KillPeep(other.gameObject);
         }
 
-        if (other.gameObject.CompareTag("Stage"))
+        if (other.gameObject.CompareTag("Target") && !hadExplode)
         {
-            DestroyAsteroid();
+            hadExplode = true;
+            Explode();
+            Destroy(other.gameObject);
         }
+        
     }
 
-   
+    private void Explode()
+    {
+        var transform1 = transform;
+        currExplosion = Instantiate(explosionEffect, transform1.position, transform1.rotation);
+        
+    }
 
     private void DestroyAsteroid()
     {
+        Destroy(currExplosion);
         Destroy(this.gameObject);
     }
 
--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -6,34 +6,81 @@ using Random = UnityEngine.Random;
 
 public class AsteroidSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] private float timeToSpawn = 1f;
-    private float _timer;
+    private GameObject[] Peeps;
+    [SerializeField] private float timeToSpawnTarget = 3f;
+    private float timerTarget;
     [SerializeField] private List<GameObject> asteroids;
     [SerializeField] private GameObject asteroidCloneFather;
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject _target;
+    private Vector3 currTargetPosition;
+
+    private bool spawnAsteroid;
+    [SerializeField] private float timeDifferenceTargetAsteroid  = .3f;
+    private float timerAsteroid;
 
     // Update is called once per frame
     void Update()
     {
-        _timer -= Time.deltaTime;
-        if (_timer <= 0)
+        timerTarget -= Time.deltaTime;
+        if (timerTarget <= 0)
         {
-            _timer = timeToSpawn;
-            var index = Random.Range(0, asteroids.Capacity);
-            var currAsteroid = asteroids[index];
-            var pos = PositionToSpawn();
-            var newAstro = Instantiate(currAsteroid, pos, quaternion.identity);
-            newAstro.transform.SetParent(asteroidCloneFather.transform);
+            timerTarget = timeToSpawnTarget;
+            SpawnTarget();
         }
+
+        if (spawnAsteroid)
+        {
+            timerAsteroid -= Time.deltaTime;
+            if (timerAsteroid <= 0)
+            {
+                SpawnAsteroid();
+            }
+        }
+        
         
     }
 
-    private Vector3 PositionToSpawn()
+
+    private void SpawnTarget()
     {
-        return transform.position;
+        PositionToSpawnTarget();
+        Instantiate(_target, currTargetPosition, Quaternion.identity);
+        spawnAsteroid = true;
+        timerAsteroid = timeDifferenceTargetAsteroid;
+    }
+    
+    private void SpawnAsteroid()
+    {
+        var index = Random.Range(0, asteroids.Capacity);
+        var currAsteroid = asteroids[index];
+        var pos = transform.position;
+        var newAstro = Instantiate(currAsteroid, pos, quaternion.identity);
+        newAstro.transform.SetParent(asteroidCloneFather.transform);
+        
+        var direction = currTargetPosition - gameObject.transform.position;
+        newAstro.GetComponent<Rigidbody>().velocity = 1 * direction;
+        spawnAsteroid = false;
+    }
+
+    private void PositionToSpawnTarget()
+    {
+        currTargetPosition = Vector3.zero;
+        var counter = 0f;
+        Peeps = GameObject.FindGameObjectsWithTag("Peep");
+
+        foreach (var peep in Peeps)
+        {
+            currTargetPosition += peep.transform.position;
+            counter += 1;
+        }
+
+        if (counter ==0)
+        {
+            currTargetPosition = Vector3.zero;
+            return;
+        }
+
+
+        currTargetPosition = new Vector3(currTargetPosition.x / counter, 0.01f, currTargetPosition.z / counter);
     }
 }
